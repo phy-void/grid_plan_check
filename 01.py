ce35f5bee23a3c7224f075b8c65e9c24e0f817eb
phy-void <phw19@qq.com>
2021-01-19 22:16:00 +0800

    Create 01.py
    Function:
    read .txt file;
    check head&end, 9 command in total.

---
+++ 01.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+txt_file_path = './'
+input_name = 'tg_20210120T01h00m30s.txt'
+txt_file_name = txt_file_path + input_name
+txt_file = open(txt_file_name, 'r')
+txt_lines = txt_file.readlines()
+txt_file.close()
+
+txt_contents = []
+command_index = 0
+for i in range(len(txt_lines)):
+    if i % 2 == 0:
+        line = txt_lines[i].split(', ')
+        line[-1] = line[-1].rstrip('\n')
+        txt_contents.append(line)
+    else:
+        txt_contents[command_index].append(txt_lines[i].rstrip('\n'))
+        command_index += 1
+    pass
+
+print('checking structure...')
+
+
+def structure_check(txt_content):
+    dictionary_head = ['pobc_on', 'enable_saving_kpack', 'enable_saving_telemetry',
+                       'enable_saving_status', 'star_tracker_on']
+    dictionary_end = ['star_tracker_off', 'disable_saving_status',
+                      'disable_saving_telemetry', 'disable_saving_kpack']
+    head = txt_content[0:5]
+    end = txt_content[-4:]
+
+    for str in dictionary_head:
+        n = 0
+        for line in head:
+            n += line.count(str)
+            pass
+        if n != 1:
+            return False
+        pass
+
+    for str in dictionary_end:
+        n = 0
+        for line in end:
+            n += line.count(str)
+            pass
+        if n != 1:
+            return False
+
+    return True
+
+
+structure_bool = structure_check(txt_contents)
+print(structure_bool)
+
+# print(np.array(txt_contents))
