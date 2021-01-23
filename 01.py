798a27d2ea5bf9cfae18cf1310bb4a5f35d40278
phy-void <phw19@qq.com>
2021-01-23 21:43:37 +0800

    Complete function: read orbit time

--- 01.py
+++ 01.py
@@ -14,6 +14,7 @@ for i in range(len(txt_lines)):
     if i % 2 == 0:
         line = txt_lines[i].split(', ')
         line[-1] = line[-1].rstrip('\n')
+        line[-1] = line[-1].rstrip('\x00')  # ???
         txt_contents.append(line)
     else:
         txt_contents[command_index].append(txt_lines[i].rstrip('\n'))
@@ -61,7 +62,32 @@ print(structure_bool)
 
 
 def orbit_identification(txt_content):
-    
-    return 0
+    orbit_start_time = []
+    observe_off_time = []
+    data_off_time = []
+    attitude_quaternion = []
+    for line in txt_content:
+        if line[2] == 'load_bin_file tg_PowerOnM.bin' or line[2] == 'load_bin_file tg_PowerOn.bin':
+            orbit_start_time.append(line[1])
+        if line[2] == 'load_bin_file tg_TXDataOn.bin':
+            observe_off_time.append(line[1])
+            line_index = txt_content.index(line)
+            next_line = txt_content[line_index + 1][2].split(' ')
+            if next_line[0] == 'upload_quaternion':
+                attitude_quaternion.append([float(next_line[1]), float(next_line[2]), float(next_line[3])])
+            else:
+                attitude_quaternion.append([None, None, None])
+        if line[2] == 'load_bin_file tg_TXDataOff.bin':
+            data_off_time.append(line[1])
 
-print(np.array(txt_contents))
+
+    return orbit_start_time, observe_off_time, data_off_time, attitude_quaternion
+
+
+#print(np.array(txt_contents))
+orbit_start_time, observe_off_time, data_off_time, attitude_quaternion=orbit_identification(txt_contents)
+print(len(orbit_start_time))
+print(len(observe_off_time))
+print(np.shape(attitude_quaternion))
+
+
